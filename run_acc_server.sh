047f49e7abeb24bfce55e84ab4ecbbe1501b0229
Joni Turunen <joni.turunen@triplan.fi>
2020-08-16 12:51:41 +0300

    task: Add startup script

---
+++ run_acc_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm ./cfg/settings.json
+adminpass=`cat admin.pass`
+specpass=`cat spectator.pass`
+echo "admin pass set: $adminpass"
+echo "spectator pass set: $specpass"
+tmp=$(mktemp)
+jq --arg a "$adminpass" '.adminPassword = $a' ./cfg/settings_template.json > "$tmp" && mv "$tmp" temp.json
+jq --arg a "$specpass" '.spectatorPassword = $a' temp.json > ./cfg/settings.json && rm temp.json
+
+echo "Starting ACC Server"
+wine ./accServer.exe
